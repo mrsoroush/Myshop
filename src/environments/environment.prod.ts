0c44bb126555fd6f76d02d906149da4247318d72
mr.soroush1988@gmail.com <mr.soroush1988@gmail.com>
2019-05-11 13:24:32 +0430

    Including Firebase to app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB6awFlnTwNOnxMB_rSMTXBcOpjC56z6dA",
+    authDomain: "myshop-c9086.firebaseapp.com",
+    databaseURL: "https://myshop-c9086.firebaseio.com",
+    projectId: "myshop-c9086",
+    storageBucket: "myshop-c9086.appspot.com",
+    messagingSenderId: "972687870458",
+    appId: "1:972687870458:web:540e573c1f089097"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB6awFlnTwNOnxMB_rSMTXBcOpjC56z6dA",
+    authDomain: "myshop-c9086.firebaseapp.com",
+    databaseURL: "https://myshop-c9086.firebaseio.com",
+    projectId: "myshop-c9086",
+    storageBucket: "myshop-c9086.appspot.com",
+    messagingSenderId: "972687870458",
+    appId: "1:972687870458:web:540e573c1f089097"
+  }
 };
 
 /*
